eb198c5e005e7788c165ca83a383d1c5fc62292a
MisakaTAT <i@mikuac.com>
2026-06-09 15:26:45 +0800

    chore: migrate project workflows to pnpm

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'src-tauri', '.yarn', 'node_modules']),
+  globalIgnores(['dist', 'src-tauri', 'node_modules']),
 
   {
     files: ['**/*.{ts,tsx}'],
